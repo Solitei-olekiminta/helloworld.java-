98df6e9007656a6ff4b02ef8d08319bebc0c1e79
solitei-peter <162104196+Solitei-olekiminta@users.noreply.github.com>
2025-03-06 14:28:42 +0300

    Create finallyBlock.java
    An example of java Exception handling using finally block

---
+++ finallyBlock.java
@@ -0,0 +1,14 @@
+public class finallyBlock {
+    public static void main(String[] args){
+        try{
+            //code that generate exception
+            int divideByZero = 5/0;
+        }
+        catch(ArithmeticException e){
+            System.out.println("ArithmeticException =>" +e.getMessage());
+        }
+        finally{
+            System.out.println("This is the finally block");
+        }
+    }
+}
